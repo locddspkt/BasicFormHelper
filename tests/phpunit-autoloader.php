28021e76611be1b1fc36faaf9f7f114cf3fd057a
LOC, Doan Dinh <locddspkt@gmail.com>
2017-12-19 11:41:18 +0700

    Finished generating select tag

--- tests/phpunit-autoloader.php
+++ tests/phpunit-autoloader.php
@@ -1,7 +1,6 @@
 <?php
 $mapping = array(
     'BasicFormHelper\TestUtils' => __DIR__ . '/TestUtils.php',
-    'BasicFormHelper\CommonFunction' => __DIR__ . '/CommonFunction.php',
 );
 
 spl_autoload_register(function ($class) use ($mapping) {
